71906ebf48e594969ae0f67b4366eca35e051b5d
AtharvaYR <87003833+AtharvaYR@users.noreply.github.com>
2021-07-06 10:26:03 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+
+ box = createSprite(150,200,20,20);
+ box.shapecolour = "blue";
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+}
+
+
+
+
